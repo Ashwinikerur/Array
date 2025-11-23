1de90478b3fc5db31c4258683aef4264c9ae9f3e
ASHWINI.KERUR <01fe24bca235@kletech.ac.in>
2025-11-23 08:01:40 +0530

    Abc branch of Array of max and min

--- Array.py
+++ Array.py
@@ -10,10 +10,15 @@ def main():
         
         total = sum(numbers)
         average = total / len(numbers)
+        maximum = max(numbers)
+        minimum = min(numbers)
 
         print("Scores:", numbers)
         print("Sum:", total)
         print("Average:", average)
+        print("Maximum:", maximum)
+        print("Minimum:", minimum)
+
 
     except ValueError:
         print("Error: Please enter only numbers separated by spaces")
